3aab60fda7c0e305738c4678c52c2bdc94b45531
roy rinberg <royrinberg@gmail.com>
2023-01-06 18:26:42 -0500

    reproduce table 1 again

--- experiment_plans/set_6_reviewer_response/run_single_experiment.sh
+++ experiment_plans/set_6_reviewer_response/run_single_experiment.sh
@@ -8,8 +8,9 @@ set -x
 
 
 ### Params that don't changes:
-BASE_OUT_DIR=$HOME/code/data/out/
 CODE_DIR=$HOME/code/personalized-pate/per-point-pate
+CODE_DIR=$HOME/code/individualized-pate-pets-submission-
+
 INPUT_DATA_DIR=$HOME/code/data/inputs/
 YAML_DIR=$CODE_DIR/experiment_plans/set_6_reviewer_response/
 
@@ -18,12 +19,21 @@ EXPERIMENT_TYPE=${1:-"mnist_upsample_figure_2"}
 YAML_DIR=${2:-$YAML_DIR}
 TIME_TO_SLEEP=${3:-0} # 7200 is 2 hours, in seconds
 OUT_SUFFIX=${4:-}
+BASE_OUT_DIR=${5:-${HOME}/code/data/out/}
 OUT_DIR=$BASE_OUT_DIR/${EXPERIMENT_TYPE}__${OUT_SUFFIX}
+
 mkdir -p $OUT_DIR    
 
 echo $EXPERIMENT_TYPE
 echo "Experiment dir : $YAML_DIR/$EXPERIMENT_TYPE.yaml" 
 
+#source /h/321/royrinberg/miniconda3/bin/activate
+source /h/321/royrinberg/.cache/pypoetry/virtualenvs/individualized-pate-4IqngVQy-py3.9/bin/activate
+
+#which python
+
+#exit 0
+#conda activate py39
 
 EXECUTABLE="ppp-run -p $YAML_DIR/$EXPERIMENT_TYPE.yaml -o $OUT_DIR -d $INPUT_DATA_DIR"
 
